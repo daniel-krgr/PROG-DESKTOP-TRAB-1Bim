a76ed756295d7d5444b5834623cee6836506db88
daniel-krgr <daniel-krugerdeal@hotmail.com>
2025-03-30 17:46:04 -0300

    Initial commit
    Estrutura inicial do projeto para gerenciamento de ordens de serviço.

---
+++ OSMecanica/src/main/java/br/unipar/osmecanica/OSMecanica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unipar.osmecanica;
+
+/**
+ *
+ * @author Daniel
+ */
+public class OSMecanica {
+
+    public static void main(String[] args) {
+        new OSMecanicaFrame().setVisible(true);
+    }
+}
---
+++ OSMecanica/src/main/java/br/unipar/osmecanica/model/Servico.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.unipar.osmecanica.model;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Servico {
+    
+    private String nome;
+    private String descricao;
+    private double preco;
+
+    public Servico() {
+    }
+
+    public Servico(String nome, String descricao, double preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "servico{" + "nome=" + nome + ", descricao=" + descricao + ", preco=" + preco + '}';
+    }
+    
+    
+    
+    
+}
